3db5ff33cad5059bca278f7a5e1cafc9e045098d
dmtzpr <dmitry.zaprutsky@gmail.com>
2017-07-27 00:48:07 +0300

    update example

--- MyBudget.Client.React-Redux/src/actions/actions.js
+++ MyBudget.Client.React-Redux/src/actions/actions.js
@@ -22,7 +22,7 @@ export function invalidateReddit(reddit) {
 function requestPosts(reddit) {
     return {
         type: REQUEST_POSTS,
-        reddit
+        reddit,
     };
 }
 
@@ -31,17 +31,17 @@ function receivePosts(reddit, json) {
         type: RECEIVE_POSTS,
         reddit,
         posts: json.data.children.map(child => child.data),
-        receivedAt: Date.now()
+        receivedAt: Date.now(),
     };
 }
 
 function fetchPosts(reddit) {
-    return dispatch => {
+    return (dispatch) => {
         dispatch(requestPosts(reddit));
         return fetch(`http://www.reddit.com/r/${reddit}.json`)
             .then(req => req.json())
             .then(json => dispatch(receivePosts(reddit, json)));
-    }
+    };
 }
 
 function shouldFetchPosts(state, reddit) {
@@ -50,9 +50,9 @@ function shouldFetchPosts(state, reddit) {
         return true;
     } else if (posts.isFetching) {
         return false;
-    } else {
-        return posts.didInvalidate;
     }
+
+    return posts.didInvalidate;
 }
 
 export function fetchPostsIfNeeded(reddit) {
@@ -60,5 +60,7 @@ export function fetchPostsIfNeeded(reddit) {
         if (shouldFetchPosts(getState(), reddit)) {
             return dispatch(fetchPosts(reddit));
         }
+
+        return null;
     };
 }
