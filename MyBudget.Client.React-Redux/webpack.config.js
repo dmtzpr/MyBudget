e2b32b6052e3db59a0e46ca38e9dfe0d4fa5f1fa
dmtzpr <dmitry.zaprutsky@gmail.com>
2017-10-30 23:27:16 +0300

    update webpack

--- MyBudget.Client.React-Redux/webpack.config.js
+++ MyBudget.Client.React-Redux/webpack.config.js
@@ -1,30 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, 'src'),
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+          },
         ],
-    },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
 };
-
