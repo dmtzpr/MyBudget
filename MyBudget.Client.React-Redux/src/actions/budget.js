12931343618e92ea84b660f4c70570fedbdc092c
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-01-16 02:23:53 +0300

    dispatch history

--- MyBudget.Client.React-Redux/src/actions/budget.js
+++ MyBudget.Client.React-Redux/src/actions/budget.js
@@ -1,7 +1,8 @@
+import { push } from 'react-router-redux';
+
 import ActionTypes from '../constants/budget';
-import { history } from '../helpers/history';
 
 export const setMonthBudget = monthBudget => (dispatch) => {
     dispatch({ type: ActionTypes.SET_MONTH_BUDGET_SUCCESS, monthBudget });
-    history.push('/');
+    dispatch(push('/'));
 };
