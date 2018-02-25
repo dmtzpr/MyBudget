f9e047b2ce23038fb39b163a33a0e99622602fff
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-02-25 03:10:35 +0300

    repush koa server

---
+++ MyBudget.Server.NodeJS-Koa/app/modules/budget/index.js
@@ -0,0 +1,12 @@
+import Router from 'koa-router';
+import budgetController from './controllers/budget-controller';
+import checkUser from '../../handlers/checkUser';
+import Budget from './models/budget';
+
+const router = new Router({ prefix: '/budget' });
+
+router.post('/', checkUser(), budgetController.create).put('/:id', checkUser(), budgetController.update);
+
+export { Budget };
+
+export default router.routes();
