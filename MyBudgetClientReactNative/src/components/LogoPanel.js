b5d55b90f37d4d6be3622f4aedd467b86a488d28
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-03-11 01:22:46 +0300

    added MyBudgetClientReactNative app

---
+++ MyBudgetClientReactNative/src/components/LogoPanel.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.logo} source={require('../images/budget.png')} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 118,
+        height: 118,
+    },
+});
