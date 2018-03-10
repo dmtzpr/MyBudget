7a94a15f8388b490584bbea51a2478b15442d92d
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-03-11 02:22:59 +0300

    use LogoPanel in app

--- MyBudgetClientReactNative/App.js
+++ MyBudgetClientReactNative/App.js
@@ -1,11 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+
+import LogoPanel from './src/components/LogoPanel';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,6 +16,7 @@ export default class App extends Component<Props> {
                 <Text style={styles.welcome}>Welcome to React Native!</Text>
                 <Text style={styles.instructions}>To get started, edit App.js</Text>
                 <Text style={styles.instructions}>{instructions}</Text>
+                <LogoPanel />
             </View>
         );
     }
