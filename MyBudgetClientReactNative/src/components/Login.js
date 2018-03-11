0c2340fbeff2e487bbf6d2f936754c6125dd1694
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-03-12 00:27:01 +0300

    update login component

--- MyBudgetClientReactNative/src/components/Login.js
+++ MyBudgetClientReactNative/src/components/Login.js
@@ -1,8 +1,74 @@
 import React, { Component } from 'react';
-import { Image, ScrollView, Platform, StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import LogoPanel from './LogoPanel';
 import FooterPanel from './FooterPanel';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+
+    content: {
+        alignItems: 'center',
+        marginTop: 50,
+        marginBottom: 50,
+    },
+
+    signInForm: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+
+    inputBox: {
+        width: 300,
+        backgroundColor: '#fff',
+        borderRadius: 6,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#333',
+        borderWidth: 1,
+        borderColor: '#ccc',
+    },
+
+    emptyTopBorderRadius: {
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+    },
+
+    emptyBottomBorderRadius: {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+
+    signInButton: {
+        width: 300,
+        backgroundColor: '#fff',
+        borderColor: '#ccc',
+        borderRadius: 6,
+        marginVertical: 10,
+        paddingVertical: 10,
+    },
+    signInButtonText: {
+        fontSize: 18,
+        fontWeight: '400',
+        color: '#333',
+        textAlign: 'center',
+    },
+    signUpButton: {
+        width: 300,
+    },
+
+    signUpButtonText: {
+        color: '#337ab7',
+        textAlign: 'right',
+        paddingHorizontal: 10,
+    },
+});
 
 export default class Login extends Component {
     render() {
@@ -11,24 +77,38 @@ export default class Login extends Component {
                 <ScrollView>
                     <View style={[styles.content]}>
                         <LogoPanel />
+                        <View style={styles.signInForm}>
+                            <TextInput
+                                style={[styles.inputBox, styles.emptyBottomBorderRadius]}
+                                underlineColorAndroid='rgba(0,0,0,0)'
+                                placeholder='Username'
+                                returnKeyType='next'
+                                placeholderTextColor='#999'
+                                autoCorrect={false}
+                                inlineImageLeft='search_icon'
+                                onSubmitEditing={() => this.passwordInput.focus()}
+                            />
+                            <TextInput
+                                style={[styles.inputBox, styles.emptyTopBorderRadius]}
+                                underlineColorAndroid='rgba(0,0,0,0)'
+                                placeholder='Password'
+                                returnKeyType='go'
+                                secureTextEntry={true}
+                                placeholderTextColor='#999'
+                                autoCorrect={false}
+                                ref={input => (this.passwordInput = input)}
+                            />
+                            <TouchableOpacity style={styles.signInButton}>
+                                <Text style={styles.signInButtonText}>Sign in</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.signUpButton}>
+                                <Text style={styles.signUpButtonText}>Register</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 </ScrollView>
-
                 <FooterPanel />
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-    },
-    content: {
-        alignItems: 'center',
-        marginTop: 50,
-        marginBottom: 50,
-    },
-});
