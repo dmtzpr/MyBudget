d160d5e199087b6f55e8518d7f5c8a8a92f036f2
dmtzpr <dmitry.zaprutsky@gmail.com>
2018-03-14 02:45:06 +0300

    update store helper

--- MyBudgetClientReactNative/src/helpers/store.js
+++ MyBudgetClientReactNative/src/helpers/store.js
@@ -1,11 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { routerMiddleware } from 'react-router-redux';
-
-import { history } from './history';
 import reducer from '../reducers';
 
-const middleware = routerMiddleware(history);
-
-export const store = createStore(reducer, composeWithDevTools(applyMiddleware(middleware, thunk)));
+export const store = createStore(reducer, compose(applyMiddleware(thunk)));
